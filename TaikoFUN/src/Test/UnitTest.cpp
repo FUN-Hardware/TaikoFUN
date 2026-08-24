1700cadd95f1a6536f951b64d592de3ec16b32df
sugimoto-blyukher <ramenrice555@gmail.com>
2026-08-25 04:06:15 +0900

    feat: ChartScanner.cpp関連のユニットテストを追加

--- TaikoFUN/src/Test/UnitTest.cpp
+++ TaikoFUN/src/Test/UnitTest.cpp
@@ -1,20 +1,69 @@
 #include "pch.h"
-#include <iostream>
 #include "CppUnitTest.h"
 #include "Core/ChartScanner.h"
 
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+#include <string>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace
+{
+	class TemporaryDirectory
+	{
+	public:
+		TemporaryDirectory()
+		{
+			const auto uniqueId = std::chrono::steady_clock::now()
+				.time_since_epoch()
+				.count();
+			path_ = std::filesystem::temp_directory_path()
+				/ ("TaikoFUN_ChartScanner_" + std::to_string(uniqueId));
+			std::filesystem::create_directories(path_);
+		}
+
+		~TemporaryDirectory()
+		{
+			std::error_code error;
+			std::filesystem::remove_all(path_, error);
+		}
+
+		const std::filesystem::path& path() const
+		{
+			return path_;
+		}
+
+	private:
+		std::filesystem::path path_;
+	};
+
+	void CreateEmptyFile(const std::filesystem::path& path)
+	{
+		std::ofstream file(path);
+	}
+}
 
 namespace UnitTest
 {
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(ChartScannerTest)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(ScanChartsReturnsOnlyTjaFiles)
 		{
+			TemporaryDirectory testDirectory;
+			const auto chartPath = testDirectory.path() / "sample.tja";
+
+			CreateEmptyFile(chartPath);
+			CreateEmptyFile(testDirectory.path() / "readme.txt");
+
 			ChartScanner scanner;
-			scanner.scanCharts("C:\\Users\\dotdo\\source\\repos\\FUN-Hardware\\TaikoFUN\\TaikoFUN\\Resource\\Debug\\シャイニングスター/");
+			const auto chartFiles = scanner.scanCharts(testDirectory.path());
+
+			Assert::AreEqual<std::size_t>(1, chartFiles.size());
+			Assert::IsTrue(chartFiles.front().ChartPath == chartPath);
+			Assert::AreEqual(std::string("sample"), chartFiles.front().Id);
 		}
 	};
 }
